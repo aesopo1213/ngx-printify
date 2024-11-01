04d40bb88cb54f2054514bd328c41a271dddecf7
DESKTOP-TR8TRRU\user <aesopo12131@hotmail.com>
2024-11-02 00:21:55 +0800

    upgrade to version 1.4.1

--- src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-printify
- */
-
-export * from './lib/ngx-printify.service';
-export * from './lib/ngx-printify.directive';
-export * from './lib/ngx-printify.module';
